e4ac17d33d29927cfce0f1eab75337e6f5bf5074
david0224-bit <david2004suarez@gmail.com>
2026-05-14 12:44:23 -0500

    feat(structure): se creo la estructura base para proyectos con react, se creo los archivos, tokens, semantic y global css, ademas se instalo react router-dom y se configuro para trabajar rutas con alias

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'url'
 
+// Crear __dirname compatible con ES Modules
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'), //Cuando veas @, interpretalo como /src
+    },
+  },
 })
